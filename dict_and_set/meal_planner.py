b646cc864a888b778e6d79e7f7cfb993adf07154
Aaron Wilcox <awilco92@hotmail.com>
2022-05-24 15:22:20 -0500

    This is updating recipe in the contents.py file and incorperating them into the meal_planner.py file

--- dict_and_set/meal_planner.py
+++ dict_and_set/meal_planner.py
@@ -4,14 +4,31 @@ from contents import pantry, recipes
 display_dict = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
+
 while True:
     # Display a menu of the recipes we know how to cook
     print("Pleace choose your recipe")
     print("--------------------------")
-    for key,value in display_dict.item():
+    for key, value in display_dict.items():
         print(f"{key} - {value}")
 
     choice = input(": ")
 
     if choice == "0":
         break
+    elif choice in display_dict:
+        selected_item = display_dict[choice]
+        print(f"You have selected {selected_item}")
+        print("checking ingredients...")
+        ingredients = recipes[selected_item]
+        print(ingredients)
+        for food_item in ingredients:
+            #   if food_item not in pantry:
+            #   print(f"\tYou do not have have {food_item}")
+            #   This was my way of doing it which was correct
+            #   However the instructor did it slightly different, so
+            #   We will follow along with his method.
+            if food_item in pantry:
+                print(f"\t{food_item} OK")
+            else:
+                print(f"\tYou don't have a necessary ingredient: {food_item}")
